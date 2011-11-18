f85b494a14c081c6a451e81bf1de559a560155e4
Maxime Morin <maxime.morin7@gmail.com>
2011-11-18 19:59:45 +0100

    C'est plus joli...

---
+++ be2/main.cpp
@@ -0,0 +1,46 @@
+//      
+//      
+//     TD1-Programation C++
+//
+//      
+//      
+
+
+#include <iostream>
+#include <fstream>
+#include "objets.h"
+#include "source.h"
+
+using namespace std;
+
+int main(int argc, char **argv)
+{	
+	cout.width(6);
+	cout.precision(4);
+	
+	circuit * montage;
+	int choix=0;
+	
+		cout << "#Quel montage ?" << endl;
+		cout << "#1 - Circuit A" << endl;
+		cout << "#2 - Circuit B" << endl;
+		cin >> choix;
+
+		switch(choix){
+		case 1:
+			montage = new circuitA; 
+			break;
+		case 2:
+			montage = new circuitB;
+			break;	
+		default:
+			cout << "#Mauvaix choix" << endl;
+			return 0;
+		}
+			
+			
+	montage->circuitSolve();
+	
+	return 0;
+}
+
